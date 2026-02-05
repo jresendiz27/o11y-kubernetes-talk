69d2f21d24ab525c99d13ef2a4af985ce772957b
Juan Reséndiz <jresendiz27@gmail.com>
2026-02-05 10:38:19 -0600

    feat: use docker engine instead of kvm2

--- bin/start_minikube.sh
+++ bin/start_minikube.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
+set -euo pipefail
+echo "Stopping existing cluster"
+minikube stop 2> /dev/null || true
+echo "Starting minikube cluster with kvm2 and Calico CNI"
 
-minikube stop
-minikube start -n 3 --cni=calico --apiserver-port=6443 --driver=kvm2
+minikube start -n 3 \
+  --cni=calico --apiserver-port=6443 \
+  --driver=docker
+
+echo "Waiting for Calico to be ready..."
+kubectl wait --for=condition=ready pods -l k8s-app=calico-node -n kube-system --timeout=300s
+echo "Enabling addons..."
+
 minikube addons enable metrics-server
 minikube addons enable storage-provisioner
 minikube status
