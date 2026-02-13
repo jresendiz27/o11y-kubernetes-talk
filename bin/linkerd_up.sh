ade13e3727fde6464ec7f7716507ca49b6b3c2e0
Juan Reséndiz <jresendiz27@gmail.com>
2026-02-13 16:45:22 -0600

    feat: fix linkerd and loki intialization

--- bin/linkerd_up.sh
+++ bin/linkerd_up.sh
@@ -8,15 +8,19 @@ if ! command -v linkerd >/dev/null 2>&1; then
   exit 1
 fi
 
+
+echo "Installing API Gateway CRDs prior installing Linkerd"
+kubectl apply --server-side -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.4.0/standard-install.yaml
+
 echo "Running linkerd preflight checks..."
 linkerd version
 linkerd check --pre
 
 echo "Installing Linkerd CRDs..."
-linkerd install --crds | kubectl apply -f -
+linkerd --set proxyInit.runAsRoot=true install --crds | kubectl apply -f -
 
 echo "Installing Linkerd control-plane..."
-linkerd install | kubectl apply -f -
+linkerd --set proxyInit.runAsRoot=true install | kubectl apply -f -
 
 echo "Waiting for Linkerd to become ready..."
 linkerd check
